1a39be04153bc997e772292b29df833e7f06f123
ozgurguclu <64900481+ozgurguclu@users.noreply.github.com>
2023-05-20 00:55:56 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { Id = 1, BrandId = 1, ColorId = 1, ModelYear = 2018, DailyPrice = 5250, Description = "Description"},
+                new Car { Id = 2, BrandId = 2, ColorId = 2, ModelYear = 2012, DailyPrice = 3500, Description = "Description"},
+                new Car { Id = 3, BrandId = 3, ColorId = 3, ModelYear = 2015, DailyPrice = 4500, Description = "Description"},
+                new Car { Id = 4, BrandId = 4, ColorId = 4, ModelYear = 2022, DailyPrice = 6500, Description = "Description"},
+                new Car { Id = 5, BrandId = 6, ColorId = 5, ModelYear = 2021, DailyPrice = 6000, Description = "Description"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
