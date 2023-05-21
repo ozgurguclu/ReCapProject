4592be0ced9b93bd81f456050f829aed1a84e837
ozgurguclu <64900481+ozgurguclu@users.noreply.github.com>
2023-05-21 13:09:51 +0300

    21.05.2023

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba eklendi";
+        public static string CarNameInvalid = "Araba ismi geçersiz";
+        public static string CarsListed = "Arabalar listelendi";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string CarNameAlreadyExists = "Aynı isimde zaten başka bir araba var";
+    }
+}
